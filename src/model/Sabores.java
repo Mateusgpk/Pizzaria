f2df6d153d214c7f06194f8aea4168a305262bcd
Nicolas Mendes Fernandes <nicolasmendes512@gmail.com>
2026-06-21 19:24:11 -0300

    Estruturando o projeto em MVC

---
+++ src/model/Sabores.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Sabores {
+    private TipoSabor tipoSabor;
+    private String nome;
+
+    public TipoSabor getTipoSabor() {
+        return tipoSabor;
+    }
+
+    public void setTipoSabor(TipoSabor tipoSabor) {
+        this.tipoSabor = tipoSabor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/model/TipoSabor.java
@@ -0,0 +1,22 @@
+package model;
+
+public class TipoSabor {
+    private String nome;
+    private Double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
